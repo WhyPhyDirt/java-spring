1e2fa1f06679927caf3aa3790bf07d3fedf0cb0d
Paul Masajjage K <pmasajjage@gmail.com>
2022-02-23 02:35:47 +0200

    Spring Initializer
    1. start.spring.io
    2. web, jpa, h2
    3. using web to generate starter spring app and importing using pom file.

---
+++ StarterWebApp/src/main/java/com/whyphydirt/StarterWebApp/StarterWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.whyphydirt.StarterWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterWebAppApplication.class, args);
+	}
+
+}
---
+++ StarterWebApp/src/test/java/com/whyphydirt/StarterWebApp/StarterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyphydirt.StarterWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
